540c2fe64d1fb1957bcffff3394ae3c6eea82d9b
Santanu Mudi <santanumudi8@gmail.com>
2023-12-21 18:29:49 +0530

    Changes in Person class

--- resume-manager/src/main/java/com/example/entity/Person.java
+++ resume-manager/src/main/java/com/example/entity/Person.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,7 +26,7 @@ public class Person implements Serializable{
 	@Column(name = "NAME")
 	private String name;
 	
-	@OneToMany
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "person")
 	private List<Resume> resumes;
 
 	public Long getId() {
